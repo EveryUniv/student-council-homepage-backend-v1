d575019c7a20108b03fdda610c32747f3b0e84ce
윤승환 <seoung59@gmail.com>
2022-07-02 14:27:23 +0900

    1st commit

---
+++ src/main/java/com/rtsoju/dku_council_homepage/DkuCouncilHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.rtsoju.dku_council_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkuCouncilHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkuCouncilHomepageApplication.class, args);
+	}
+
+}
