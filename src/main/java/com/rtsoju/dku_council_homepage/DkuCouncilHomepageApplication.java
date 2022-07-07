6fea05bee0c99703430387e872a6c3713d0c85d4
Seungyong Choi <ppyooy336@naver.com>
2022-07-07 14:17:53 +0900

    Feat : seperate properties

--- src/main/java/com/rtsoju/dku_council_homepage/DkuCouncilHomepageApplication.java
+++ src/main/java/com/rtsoju/dku_council_homepage/DkuCouncilHomepageApplication.java
@@ -2,13 +2,23 @@ package com.rtsoju.dku_council_homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing //jpa에서 auditing을 자동으로 시켜주기 위해서.. 업데이트용도
 public class DkuCouncilHomepageApplication {
 
+
+//	public static final String APPLICATION_LOCATIONS =
+//			"spring.config.location="
+//					+ "classpath:application.properties,"
+//					+ "classpath:application-smtp.properties,";
+
 	public static void main(String[] args) {
+//		new SpringApplicationBuilder(DkuCouncilHomepageApplication.class)
+//				.properties(APPLICATION_LOCATIONS)
+//				.run(args);
 		SpringApplication.run(DkuCouncilHomepageApplication.class, args);
 	}
 
