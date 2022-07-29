4a11c6f69b9e0dfb3805b4d3e2d9383748c21ef1
YOON <seoung59@gmail.com>
2022-07-29 22:51:44 +0900

    Fix: RequestPostDto mapping

--- src/main/java/com/rtsoju/dku_council_homepage/domain/post/entity/dto/request/RequestNewsDto.java
+++ src/main/java/com/rtsoju/dku_council_homepage/domain/post/entity/dto/request/RequestNewsDto.java
@@ -10,17 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class RequestNewsDto {
-
-    @NotBlank(message = "title(제목)은 비어있으면 안됩니다.")
-    private String title;
-
-    @NotBlank(message = "text(본문)이 비어있으면 안됩니다.")
-    private String text;
-
-    private List<MultipartFile> files = new ArrayList<>();
-
+public class RequestNewsDto extends RequestPostDto{
     public News toNewsEntity() {
-        return new News(title, text);
+        return new News(super.getTitle(), super.getText());
     }
 }
