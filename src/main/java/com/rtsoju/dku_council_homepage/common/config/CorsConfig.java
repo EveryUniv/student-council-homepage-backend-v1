a6cc9cd5b0e5b7fbecce52f5b34e435c121b644c
YOON <seoung59@gmail.com>
2022-11-11 20:58:01 +0900

    Fix: CorsFileter

--- src/main/java/com/rtsoju/dku_council_homepage/common/config/CorsConfig.java
+++ src/main/java/com/rtsoju/dku_council_homepage/common/config/CorsConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
 
@@ -15,10 +17,12 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-//
+//        config.addAllowedOriginPattern("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
 //        config.setAllowedOrigins("*"); // 모든 Ip에 응답을 허용
 //        config.setAllowedHeaders("*"); // 모든 header에 응답을 허용
 //        config.setAllowedMethods("*"); // 모든 post, get ,put 등등 요청을 허용
