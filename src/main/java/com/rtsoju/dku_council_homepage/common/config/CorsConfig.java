bef7a7966a028f132e7a51e704f1697e0603055c
YOON <seoung59@gmail.com>
2023-01-17 14:07:06 +0900

    Fix : CorsFilter

--- src/main/java/com/rtsoju/dku_council_homepage/common/config/CorsConfig.java
+++ src/main/java/com/rtsoju/dku_council_homepage/common/config/CorsConfig.java
@@ -1,35 +1,19 @@
 package com.rtsoju.dku_council_homepage.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addExposedHeader("Set-Cookie");
-//        config.setAllowedOriginPatterns(Arrays.asList("*"));
-//        config.setAllowedHeaders(Arrays.asList("*"));
-//        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
-//        config.setAllowedOrigins("*"); // 모든 Ip에 응답을 허용
-//        config.setAllowedHeaders("*"); // 모든 header에 응답을 허용
-//        config.setAllowedMethods("*"); // 모든 post, get ,put 등등 요청을 허용
-        source.registerCorsConfiguration("/api/**", config);
-        return new CorsFilter(source);
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
